9c0649d4d13c2cf6cc6517f13717b07195c25f85
Jonas Bucher <j.bucher.mn@gmail.com>
2022-03-31 09:04:37 +0200

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "NixOS simple deploy";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, pywmpkg, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      packages.nixos-simple-deploy =
+        pkgs.python3.pkgs.buildPythonApplication rec {
+          pname = "nixos-simple-deploy";
+          version = "0.1";
+
+          src = ./.;
+
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            spur
+
+            setuptools
+          ];
+
+          setuptoolsCheckPhase = "true";
+        };
+
+      devShell = let
+        my-python = pkgs.python3;
+        python-with-my-packages = my-python.withPackages (ps: with ps; [
+          spur
+        ]);
+      in
+        pkgs.mkShell {
+          buildInputs = [ python-with-my-packages ];
+        };
+    }
+  );
+}
